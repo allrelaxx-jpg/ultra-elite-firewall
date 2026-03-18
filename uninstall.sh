e16e7aee8dfd05d0d7a4e7785345eaa9201c7289
allrelaxx-jpg <allrelaxx@gmail.com>
2026-03-18 17:06:00 +0300

    Modify uninstall script for firewall removal
    Updated uninstall script to remove firewall configuration files more safely.

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-echo "🧨 Removing firewall..."
-
 systemctl stop nftables
 systemctl disable nftables
 
-rm -rf /etc/nftables.conf
-rm -rf /etc/nftables
+rm -f /etc/nftables.conf
 
-echo "Done"
+echo "Firewall removed"
